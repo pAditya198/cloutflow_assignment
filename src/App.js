1632f6e7c8a410245694716ffcb4380524817832
Aditya Prakash <p.aditya.198@gmail.com>
2022-06-02 18:56:59 +0530

    data fetching

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [data, setData] = useState([]);
+
+  const fetchMultipleData = (index) => {
+    if (index >= 20) {
+      return;
+    }
+    const endpoints = [];
+    for (let i = 1; i <= 5; i++) {
+      endpoints.push(
+        "https://jsonplaceholder.typicode.com/posts/" + (index * 5 + i)
+      );
+    }
+
+    Promise.allSettled(endpoints.map((api) => axios.get(api))).then((data) => {
+      setData((prev) => {
+        return [...prev, ...data.map((res) => res.value.data)];
+      });
+      fetchMultipleData(index + 1);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="button" onClick={() => fetchMultipleData(0)}>
+        Click to start fetching
+      </button>
+      {data.map((ele) => {
+        return <p>{ele.id}</p>;
+      })}
     </div>
   );
 }
-
-export default App;
